e801161f1b7f0f053c7019872b9f2b15d3233115
Arsole <90244149+Arsole676@users.noreply.github.com>
2023-02-28 06:23:19 +0530

    path changed

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -2,7 +2,10 @@ import path from 'path';
 import { nodeResolvePlugin } from 'esbuild-node-resolve';
 
 export default {
-  entryPoints: [path.resolve(__dirname ,'./src/index.js')],
+  entryPoints: [
+    path.resolve(__dirname , './src/index.js'),
+    path.resolve(__dirname, 'test/test.js')
+],
   outfile: path.resolve(__dirname ,'./dist/mainBuild.min.js'),
   minify: true,
   bundle: true,
